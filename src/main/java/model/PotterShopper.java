b5c0879a36adbd19a26ff720dedbc00d920b4fbd
upsd <up648421@myport.ac.uk>
2018-09-06 14:18:31 +0100

    Implemented 20% discount for four distinct books.

--- src/main/java/model/PotterShopper.java
+++ src/main/java/model/PotterShopper.java
@@ -17,10 +17,19 @@ public class PotterShopper {
             return 16.00;
         }
         if (books.size() == 3) {
-            if (!books.get(0).sameAs(books.get(1)) && !books.get(1).sameAs(books.get(2))) {
+            if (!books.get(0).sameAs(books.get(1))
+                    && !books.get(1).sameAs(books.get(2))) {
                 return 21.60;
             }
             return 24.00;
+        }
+        if (books.size() == 4) {
+            if (!books.get(0).sameAs(books.get(1))
+                    && !books.get(1).sameAs(books.get(2))
+                    && !books.get(2).sameAs(books.get(3))) {
+                return 25.60;
+            }
+            return 32.00;
         }
 
         return 0;
