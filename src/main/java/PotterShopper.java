b0bf3bdffb1256d9d46a1ea77985596d6863e426
upsd <up648421@myport.ac.uk>
2018-09-06 13:46:06 +0100

    Add simplest possible implementation to get test passing for purchase of single book.

--- src/main/java/PotterShopper.java
+++ src/main/java/PotterShopper.java
@@ -3,6 +3,9 @@ import java.util.List;
 public class PotterShopper {
 
     public double billFor(List<Book> books) {
+        if (books.size() == 1) {
+            return 8.00;
+        }
         return 0;
     }
 }
